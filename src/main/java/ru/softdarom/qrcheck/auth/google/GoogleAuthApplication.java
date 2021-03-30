8c35bdcac17e5d11dff011a2d1c62a7a1555a653
Евгения Ковтун <Kovtun.E.E@sberbank.ru>
2021-03-30 12:28:25 +0300

    REG-4 fixed some bugs

--- src/main/java/ru/softdarom/qrcheck/auth/google/GoogleAuthApplication.java
+++ src/main/java/ru/softdarom/qrcheck/auth/google/GoogleAuthApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import ru.softdarom.qrcheck.auth.google.config.property.ApiKeySecurityProperties;
 import ru.softdarom.qrcheck.auth.google.config.property.FeignClientProperties;
 
 @SpringBootApplication
 @EnableWebSecurity
-@EnableConfigurationProperties({FeignClientProperties.class, ApiKeySecurityProperties.class})
+@EnableConfigurationProperties({FeignClientProperties.class})
 public class GoogleAuthApplication {
 
     public static void main(String[] args) {
