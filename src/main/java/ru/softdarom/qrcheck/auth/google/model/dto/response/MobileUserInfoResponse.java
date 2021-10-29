5ec2f2454fb7d871061864da76272fca063039a4
Evgeniya Kovtun <eekovtun@gmail.com>
2021-10-29 12:00:21 +0300

    QRC-34 (#47)
    added accessToken in /oauth2/success
    removed HEADER_ACCESS_TOKEN_NAME header

---
+++ src/main/java/ru/softdarom/qrcheck/auth/google/model/dto/response/MobileUserInfoResponse.java
@@ -0,0 +1,54 @@
+package ru.softdarom.qrcheck.auth.google.model.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+import ru.softdarom.qrcheck.auth.google.util.JsonHelper;
+
+import java.util.Optional;
+
+@Generated
+@Data
+@NoArgsConstructor
+public class MobileUserInfoResponse {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("firstName")
+    private String firstName;
+
+    @JsonProperty("secondName")
+    private String secondName;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("accessToken")
+    private String accessToken;
+
+    public MobileUserInfoResponse(AuthHandlerUserResponse response) {
+        var notNullResponse = Optional.ofNullable(response).orElseThrow();
+        setUserInfo(notNullResponse);
+        setTokenInfo(notNullResponse);
+    }
+
+    @Override
+    public String toString() {
+        return JsonHelper.asJson(this);
+    }
+
+    private void setTokenInfo(AuthHandlerUserResponse notNullResponse) {
+        var notNullToken = Optional.ofNullable(notNullResponse.getToken()).orElseThrow();
+        this.accessToken = notNullToken.getAccessToken().getToken();
+    }
+
+    private void setUserInfo(AuthHandlerUserResponse notNullResponse) {
+        var notNullUser = Optional.ofNullable(notNullResponse.getUser()).orElseThrow();
+        this.id = notNullUser.getId();
+        this.firstName = notNullUser.getFirstName();
+        this.secondName = notNullUser.getSecondName();
+        this.email = notNullUser.getEmail();
+    }
+}
