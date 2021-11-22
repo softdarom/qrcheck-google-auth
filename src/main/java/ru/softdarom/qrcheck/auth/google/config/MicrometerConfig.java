031306e5490cd9451c989ad8fe41971ff0fe09d8
Евгения Ковтун <eekovtun@gmail.com>
2021-11-22 20:34:54 +0300

    QRC-26

--- src/main/java/ru/softdarom/qrcheck/auth/google/config/MicrometerConfig.java
+++ src/main/java/ru/softdarom/qrcheck/auth/google/config/MicrometerConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MicrometerConfig {
 
-    private static final String APPLICATION_NAME_TAG = "appliction";
+    private static final String APPLICATION_NAME_TAG = "application";
 
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCustomTags(@Value("${spring.application.name}") String applicationName) {
